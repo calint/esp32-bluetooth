e684c4aae453eaf0e6542b69180248898ab5b6ec
calint <calin.tenitchi@gmail.com>
2024-04-21 20:05:30 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@ static constexpr char const *speaker_name = "JBL Go 3";
 
 static BluetoothA2DPSource a2dp_source;
 
-static int32_t write_data_frames(Frame *frame, int32_t frame_count);
+static int32_t callback_write_data_frames(Frame *frame, int32_t frame_count);
 
 void setup() {
   a2dp_source.set_auto_reconnect(false);
-  a2dp_source.start(speaker_name, write_data_frames);
+  a2dp_source.start(speaker_name, callback_write_data_frames);
   a2dp_source.set_volume(50);
 }
 
@@ -20,7 +20,7 @@ void loop() {
 // The supported audio codec in ESP32 A2DP is SBC. SBC audio stream is encoded
 // from PCM data normally formatted as 44.1kHz sampling rate, two-channel 16-bit
 // sample data
-static int32_t write_data_frames(Frame *frame, int32_t frame_count) {
+static int32_t callback_write_data_frames(Frame *frame, int32_t frame_count) {
   static int16_t amp = 0;
 
   // ESP_LOGD("MAIN", "frame_count: %d", frame_count);
