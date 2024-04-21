a4842f9d97085f901ddcafa30acb5e82b03923df
calint <calin.tenitchi@gmail.com>
2024-04-21 17:42:22 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,7 @@
 
 BluetoothA2DPSource a2dp_source;
 
-// The supported audio codec in ESP32 A2DP is SBC. SBC audio stream is encoded
-// from PCM data normally formatted as 44.1kHz sampling rate, two-channel 16-bit
-// sample data
-int32_t get_data_frames(Frame *frame, int32_t frame_count) {
-  // ESP_LOGD("MAIN", "frame_count: %d", frame_count);
-
-  static int16_t amp = 0;
-  for (int sample = 0; sample < frame_count; ++sample) {
-    frame[sample].channel1 = amp;
-    frame[sample].channel2 = frame[sample].channel1;
-    amp += 100;
-  }
-
-  // to prevent watchdog
-  esp_task_wdt_reset();
-
-  return frame_count;
-}
+static int32_t get_data_frames(Frame *frame, int32_t frame_count);
 
 void setup() {
   a2dp_source.set_auto_reconnect(false);
@@ -30,4 +13,24 @@ void setup() {
 void loop() {
   // to prevent watchdog in release > 1.0.6
   delay(1000);
+}
+
+// The supported audio codec in ESP32 A2DP is SBC. SBC audio stream is encoded
+// from PCM data normally formatted as 44.1kHz sampling rate, two-channel 16-bit
+// sample data
+static int32_t get_data_frames(Frame *frame, int32_t frame_count) {
+  static int16_t amp = 0;
+
+  // ESP_LOGD("MAIN", "frame_count: %d", frame_count);
+
+  for (int sample = 0; sample < frame_count; ++sample) {
+    frame[sample].channel1 = amp;
+    frame[sample].channel2 = frame[sample].channel1;
+    amp += 100;
+  }
+
+  // to prevent watchdog
+  esp_task_wdt_reset();
+
+  return frame_count;
 }
